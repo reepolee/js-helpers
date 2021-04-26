0180ee06185d9d3c7c0e5ba10c6ee1ff3bee05fc
alesvaupotic <ales@vaupotic.eu>
2021-04-26 17:27:37 +0200

    0.1.4

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
-export default function isEven(num: number) {
+export default function name() {
+    return "js-helpers";
+}
+export function isEven(num: number) {
     let ret = num % 2 === 0;
     return ret;
 }
