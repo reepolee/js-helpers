86d13e7fb73689085df74a9b9a20e23c3124cd8e
alesvaupotic <ales@vaupotic.eu>
2021-04-26 17:44:04 +0200

    real funcs

--- index.ts
+++ index.ts
@@ -1,11 +1,54 @@
-export default function name() {
+export default () {
     return "js-helpers";
 }
-export function isEven(num: number) {
-    let ret = num % 2 === 0;
-    return ret;
+
+// ----------------------------------------------------------------------------
+function dateForDisplay(dateValue: string, locale: string) {
+    return new Date(dateValue).toLocaleDateString(locale);
 }
 
-export const lala = 10;
+// ----------------------------------------------------------------------------
+function log(param: any) {
+    console.log(JSON.stringify(param, null, 4));
+}
 
+// ----------------------------------------------------------------------------
+function avif(url: string, size: number) {
+    let s = url;
+    if (size) {
+        const fname = s.split("/").pop().split("/").pop();
+        s = s.replace(fname, `${size}/${fname}`);
+    }
 
+    if (s.includes("png")) {
+        return s.split(".png").join(".avif");
+    }
+    return s.split(".jpg").join(".avif");
+}
+
+// ----------------------------------------------------------------------------
+function jpeg(url: string, size: number) {
+    let s = url;
+    if (size) {
+        const fname = s.split("/").pop().split("/").pop();
+        s = s.replace(fname, `${size}/${fname}`);
+    }
+
+    return s;
+}
+
+// ----------------------------------------------------------------------------
+function webp(url: string, size: number) {
+    let s = url;
+    if (size) {
+        const fname = s.split("/").pop().split("/").pop();
+        s = s.replace(fname, `${size}/${fname}`);
+    }
+
+    if (s.includes("png")) {
+        return s.split(".png").join(".webp");
+    }
+    return s.split(".jpg").join(".webp");
+}
+
+export { dateForDisplay, log, avif, jpeg, webp };
