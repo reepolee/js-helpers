7127832a477bcf50a95ff838943bd9a9cf39a3ed
alesvaupotic <ales@vaupotic.eu>
2021-04-26 16:46:59 +0200

    init

--- index.js
+++
@@ -1,5 +0,0 @@
-const isEven = (number) => {
-    return number % 2 === 0;
-};
-
-module.exports = isEven;
---
+++ index.ts
@@ -0,0 +1,8 @@
+function isEven(num: number) {
+    let ret = num % 2 === 0;
+    return ret;
+}
+
+const lala = 10;
+
+export { isEven, lala };
